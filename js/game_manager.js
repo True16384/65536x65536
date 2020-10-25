b7176348988e30b983b803ef3135766cc456d9dc
Ryan2144 <70672438+Ryan2144@users.noreply.github.com>
2020-10-25 17:05:15 -0500

    Change goal

--- js/game_manager.js
+++ js/game_manager.js
@@ -166,8 +166,8 @@ GameManager.prototype.move = function (direction) {
           // Update the score
           self.score += merged.value;
 
-          // The mighty 2048 tile
-          if (merged.value === 2048) self.won = true;
+          // The mighty 4294967296 tile
+          if (merged.value === 4294967296) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
